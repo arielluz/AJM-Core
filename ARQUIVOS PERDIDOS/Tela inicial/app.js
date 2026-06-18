1357f068bfacda73f4fa10e94ae2da92e384f2ce
joaopaulomarasca <joao_tessaro@estudante.sesisenai.org.br>
2026-06-17 21:56:50 -0300

    Ajustei os arquivos e organizei de forma inteligente

--- ARQUIVOS PERDIDOS/Tela inicial/app.js
+++ ARQUIVOS PERDIDOS/Tela inicial/app.js
@@ -1,8 +1,7 @@
-<<<<<<< HEAD:Tela inicial/app.js
 setTimeout(() => {
     window.location.href = "principal.html";
 }, 3000); 
-=======
+
 function toggleCriaturas() {
     const menu = document.getElementById("criaturasMenu");
     const button = document.querySelector(".tab:nth-child(2)");
@@ -10,4 +9,3 @@ function toggleCriaturas() {
     menu.classList.toggle("active");
     button.classList.toggle("active");
   }
->>>>>>> 84e1306f66ea031f5cb5a42b3a6c0af563f56765:ARQUIVOS PERDIDOS/Tela inicial/app.js
