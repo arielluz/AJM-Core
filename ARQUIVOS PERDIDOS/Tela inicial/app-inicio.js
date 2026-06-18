803cbfc5603c6be3d6e65c41a01b250962b454d4
ARIEL DA LUZ ROSA <ariel_l_rosa@estudante.sesisenai.org.br>
2026-06-18 20:53:11 -0300

    Test

---
+++ ARQUIVOS PERDIDOS/Tela inicial/app-inicio.js
@@ -0,0 +1,73 @@
+const blocos = [
+    "INICIALIZANDO SISTEMA...",
+
+    "Verificando arquivos...",
+
+    "ERRO.",
+
+    "Um arquivo não registrado foi encontrado.",
+
+    "Nome: DESCONHECIDO",
+
+    "Abrindo conteúdo...",
+
+    "Se você está lendo isto, então o arquivo conseguiu ser recuperado.",
+
+    "Algo aconteceu aqui.",
+
+    "Algo que nunca deveria ter existido."
+];
+
+const container = document.getElementById("container");
+
+let blocoAtual = 0;
+
+
+const cursor = document.createElement("span");
+cursor.className = "cursor";
+cursor.textContent = "█";
+
+function escreverBloco() {
+    if (blocoAtual >= blocos.length) {
+       
+        revelarBotao();
+        return;
+    }
+
+    const p = document.createElement("div");
+    p.className = "paragrafo";
+    container.appendChild(p);
+    p.appendChild(cursor);
+
+    let letra = 0;
+
+    function digitar() {
+        if (letra < blocos[blocoAtual].length) {
+            
+            cursor.insertAdjacentText("beforebegin", blocos[blocoAtual][letra]);
+            letra++;
+            setTimeout(digitar, 40);
+        } else {
+            blocoAtual++;
+
+         
+            setTimeout(escreverBloco, 1000);
+        }
+    }
+
+    digitar();
+}
+
+escreverBloco();
+
+function revelarBotao() {
+    const btn = document.getElementById("btn-avancar");
+    if (!btn) return;
+
+    btn.classList.remove("oculto");
+    btn.classList.add("visivel");
+
+    btn.addEventListener("click", () => {
+        window.location.href = "Arquivo-Perdido.html";
+    });
+}
